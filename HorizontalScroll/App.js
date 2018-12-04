a147577935768cba30d227e34497c3fd4c84874d
Tushar <gupta.tus@husky.neu.edu>
2018-12-04 11:41:27 -0500

    horizoltal scrolling true

--- HorizontalScroll/App.js
+++ HorizontalScroll/App.js
@@ -7,7 +7,8 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View} from 'react-native'; 
+import Horizontal from './components/Horizontal';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,14 +17,12 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+
+export default class App extends Component{
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style={styles.container}>       
+        <Horizontal />
       </View>
     );
   }
---
+++ HorizontalScroll/components/Horizontal.js
@@ -0,0 +1,49 @@
+
+
+import React, {Component} from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    ScrollView,
+    Dimensions
+} from 'react-native';
+
+
+export default class Horizontal extends Component {
+  render() {
+    return (
+      <ScrollView horizontal={true} style={styles.container}>
+          <View style={styles.outer}>
+            <Text style={styles.innerText}>Hello</Text>            
+          </View>
+          <View style={styles.outer}>
+            <Text style={styles.innerText}>Hello</Text>            
+          </View>
+          <View style={styles.outer}>
+            <Text style={styles.innerText}>Hello</Text>            
+          </View>
+               
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {    
+  },
+  outer:{
+    backgroundColor: '#007bb6',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+  innerText:{
+    color: '#fff',
+    fontSize: 23,
+    fontWeight: 'bold',
+  }
+  
+});
