fb42dc139c4f8228d5f51cba3d5079d074b30451
Tushar <gupta.tus@husky.neu.edu>
2018-12-05 10:18:17 -0500

    Done Style commit

--- LoginScreen/App.js
+++ LoginScreen/App.js
@@ -33,6 +33,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#282530',
   },  
 });
