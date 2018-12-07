6922eb2e1ab542f196584721940c7180c64c7b00
Tushar <gupta.tus@husky.neu.edu>
2018-12-07 17:39:22 -0500

    Shopping cart Screen done project 3

--- ShoppingCartProjectThree/components/Header.js
+++ ShoppingCartProjectThree/components/Header.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet, Image  } from 'react-native'
+import { Text, View, StyleSheet, Image, Dimensions  } from 'react-native'
 
 export default class Header extends Component {
   render() {
     return (
       <View style={styles.header}>
         <Image 
-            source={require('../images/cart.png')}
+            source={require('../images/cart1.png')}
             style={styles.logo} 
         />
         <Text style={styles.title}>Headphones Sopping Cart</Text>
@@ -17,24 +17,32 @@ export default class Header extends Component {
 
 const styles = StyleSheet.create({
     header: {
-      flex: 1,
+      
       justifyContent: 'center',
       alignItems: 'center',
-      backgroundColor: '#F5FCFF',
-      height: 80,
+      backgroundColor: '#fff',      
       marginTop: 20,
+      height: 80,
+      padding: 20,
+      flexDirection: 'row',
+      borderBottomWidth: 4,
+      borderBottomColor: '#ccc',
+      width: Dimensions.get('window').width
     },
     logo: {
       fontSize: 20,
-      textAlign: 'center',
-      marginTop: 10,
-      width: 40,
-      height: 40
+      marginTop: 0,
+      marginLeft: -2,
+      width:50,
+      height:50,
     },
     title: {
       textAlign: 'center',
       color: '#333333',
-      marginBottom: 5,
-      fontSize: 20
+      marginBottom: 0,  
+      fontSize: 17,
+      color: '#000',
+      marginLeft: 10,
+      fontStyle: 'italic',
     },
   });
