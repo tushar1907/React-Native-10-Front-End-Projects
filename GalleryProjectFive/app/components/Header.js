c593d518ea6fc86c1afcd13a88fe92d2575e7030
Tushar <gupta.tus@husky.neu.edu>
2018-12-11 13:08:10 -0500

    Profile Pic setup for Project 5

--- GalleryProjectFive/app/components/Header.js
+++ GalleryProjectFive/app/components/Header.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View, ImageBackground } from 'react-native'
+import { Text, StyleSheet, View, ImageBackground, Image } from 'react-native'
 
 export default class Header extends Component {
   render() {
     return (
       <ImageBackground 
       style={styles.headerbg}
-      source={require('../images/img1.jpeg')}>      
+      source={require('../images/img1.jpg')}>      
+      <View style={styles.headercontainer}>
+        <View style={styles.profilepiccontainer}>
+            <Image style={styles.mypic}
+            source={require('../images/img5.jpg')} />
+
+
+        </View>
+        <Text></Text>
+      </View>
       </ImageBackground>
     )
   }
@@ -17,5 +26,25 @@ const styles = StyleSheet.create({
         flex: 1,
         width: null,
         alignSelf: 'stretch',
+    },
+    headercontainer:{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+    },
+    profilepiccontainer:{
+        width: 180,
+        height: 180,
+        
+    },
+    mypic:{
+        flex: 1,
+        width: null,
+        alignSelf: 'stretch',
+        borderRadius: 90,   
+        borderWidth: 3,
+        borderColor: '#fff',
     }
+
 })
