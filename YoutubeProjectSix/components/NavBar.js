6c1912aa30f745e9dc187b828f6976ad99b9df09
Tushar <gupta.tus@husky.neu.edu>
2018-12-21 15:20:30 -0500

    Icons on the logo places Project 6

--- YoutubeProjectSix/components/NavBar.js
+++ YoutubeProjectSix/components/NavBar.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
 import { Text, StyleSheet, View, Image } from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+
 
 export default class NavBar extends Component {
   render() {
     return (
       <View style={styles.container}>
+      
         <View style={styles.navbar}>
             <Image 
             style={styles.logo}
             source={require('./img1.jpg')}/>
+
+            <View style={styles.moveright}>
+            <Icon name="search" size={27} style={styles.icons}/>
+            <Icon name="account-circle" size={27} style={styles.icons}/>
+            </View>
         </View>
       </View>
     )
@@ -32,6 +40,12 @@ const styles = StyleSheet.create({
     logo:{
         width:120,
         height: 23,
-
+    },
+    moveright:{
+        flexDirection: 'row',
+    },
+    icons:{
+        padding: 5,
+        marginLeft: 15,
     }
 })
